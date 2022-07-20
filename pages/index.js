fa25ca148db0d96deb151e3cc9b2e88b82fc7b62
Michael Yogar <michaelyogar878@gmail.com>
2022-07-20 13:23:18 -0400

    Get posts using getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
+import { getAllPosts } from "../utils";
+
 export default function Home() {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
+
+export async function getStaticProps(context) {
+  const posts = getAllPosts();
+  console.log(posts);
+
+  return {
+    props: {},
+  };
 }
