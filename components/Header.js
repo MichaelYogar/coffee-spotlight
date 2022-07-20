c339dc785d2f910e9bbbbcee110a87966903adae
Michael Yogar <michaelyogar878@gmail.com>
2022-07-20 17:10:40 -0400

    Update header using flex

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,27 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-  return <header className="sticky top-0">Heaer </header>;
+  return (
+    <header className="top-0 bg-black/5 flex justify-around p-4">
+      <div>
+        <Link href="/">
+          <a>Michael Yogar</a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/">
+          <a className="mr-4 hover:underline underline-offset-4">Home</a>
+        </Link>
+        <Link href="/blog">
+          <a className="mr-4 hover:underline underline-offset-4">Blog</a>
+        </Link>
+        <Link href="/about">
+          <a className="hover:underline underline-offset-4">About</a>
+        </Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- components/Layout.js
+++ components/Layout.js
@@ -3,10 +3,12 @@ import Header from "./Header";
 
 const Layout = ({ children }) => {
   return (
-    <div className="max-w-prose mx-auto px-4">
+    <>
       <Header />
-      <main className="pt-4 pb-12">{children}</main>
-    </div>
+      <div className="max-w-prose mx-auto px-4">
+        <main className="pt-4 pb-12">{children}</main>
+      </div>
+    </>
   );
 };
 
