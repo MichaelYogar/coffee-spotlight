b3586944c142bf1b2efe5b21e198c0d5fa00b9dd
Michael Yogar <michaelyogar878@gmail.com>
2022-07-21 16:49:54 -0400

    Wrap image with anchor tag for nextjs icon

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,15 @@
 import { Flex } from "@chakra-ui/react";
 import Image from "next/image";
 import React from "react";
-import NextJSLogo from "../public/icons/nextdotjs.svg";
+import NextJSIcon from "../public/icons/nextdotjs.svg";
 
 const Footer = () => {
   return (
     <Flex className="tw-justify-center tw-whitespace-pre-wrap tw-my-2">
       <span>Made with 💗 and </span>
-      <Image src={NextJSLogo} alt="nextjs logo" height={30} />
+      <a href="https://nextjs.org/" target={"_blank"} rel={"noreferrer"}>
+        <Image src={NextJSIcon} alt="nextjs logo" height={30} />
+      </a>
     </Flex>
   );
 };
