709c12d1c14ff82e6e542ef80230409d53768580
Michael Yogar <michaelyogar878@gmail.com>
2022-07-21 16:41:26 -0400

    Update Heading for AboutMe component

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -4,10 +4,11 @@ import React from "react";
 const AboutMe = () => {
   return (
     <Flex flexDirection="column" justifyContent="flex-start" mb={4}>
-      <Heading as="h1" size={"2xl"} mb={1}>
+      <Heading as="h1" size={"xl"} mb={1}>
         Hi, I&apos;m Michael
         <span role="img" aria-label="Coffee">
-          ☕{" "}
+          {" "}
+          ☕
         </span>
       </Heading>
       <Text>I&apos;m a software developer that loves coffee!</Text>
