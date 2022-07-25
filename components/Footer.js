1358f8162e92e1a3d8234f2cd6e33def571f5bd1
Michael Yogar <michaelyogar878@gmail.com>
2022-07-25 11:43:50 -0400

    Update Footer icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,10 @@
-import { Flex, Link, Text } from "@chakra-ui/react";
-import Image from "next/image";
+import { Flex, Icon, Link, Text } from "@chakra-ui/react";
 import React from "react";
-import NextJSIcon from "../public/icons/nextdotjs.svg";
 
 const Footer = () => {
   return (
     <Flex justify="center" whiteSpace="pre-wrap" marginY="2">
-      <Text as="span">Made with 💗 and </Text>
-      <Link href="https://nextjs.org/" isExternal>
-        <Image src={NextJSIcon} alt="nextjs logo" height={30} />
-      </Link>
+      <Text as="span">Made with 🧊 + ☕ </Text>
     </Flex>
   );
 };
