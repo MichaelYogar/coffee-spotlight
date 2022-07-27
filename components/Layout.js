171d184e714a85be6dfbb66cbb705a43bf0de5b8
Michael Yogar <michaelyogar878@gmail.com>
2022-07-27 14:09:54 -0400

    Add custom next/head

--- components/Layout.js
+++ components/Layout.js
@@ -2,10 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import { Box, Container, Divider, Spacer } from "@chakra-ui/react";
+import Head from "next/head";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, title = "Coffee Spotlight" }) => {
   return (
     <>
+      <Head>
+        <title>{title}</title>
+        <meta name="author" content="Michael Yogar" />
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Container display="flex" px={1} height="100vh" flexDirection="column">
         <Header />
         <Box as="main">{children}</Box>
