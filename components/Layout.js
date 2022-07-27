5d5f1e84ca19f2f09963deb0110ca45a78d00784
Michael Yogar <michaelyogar878@gmail.com>
2022-07-27 14:35:26 -0400

    Fix charSet attribute

--- components/Layout.js
+++ components/Layout.js
@@ -10,14 +10,14 @@ const Layout = ({ children, title = "Coffee Spotlight" }) => {
       <Head>
         <title>{title}</title>
         <meta name="author" content="Michael Yogar" />
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Container display="flex" px={1} height="100vh" flexDirection="column">
         <Header />
         <Box as="main">{children}</Box>
         <Spacer />
-        <Divider borderBottomWidth="2px" mt={5} />
+        <Divider mt={5} />
         <Footer />
       </Container>
     </>
