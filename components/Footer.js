e3fffda4aa50a4ebc4035a8ca94815b15cd19de5
Michael Yogar <michaelyogar878@gmail.com>
2022-07-28 15:41:14 -0400

    Add link to repo

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,17 @@
 import { Flex, Icon, Link, Text } from "@chakra-ui/react";
 import React from "react";
+import NextLink from "next/link";
 
 const Footer = () => {
   return (
     <Flex justify="center" whiteSpace="pre-wrap" py={5}>
       <Text as="span">Made with 🧊 + ☕. </Text>
-      <Text as="span">View Source.</Text>
+      <NextLink
+        href={"https://github.com/MichaelYogar/coffee-spotlight"}
+        passHref
+      >
+        <Link isExternal>View Source</Link>
+      </NextLink>
     </Flex>
   );
 };
